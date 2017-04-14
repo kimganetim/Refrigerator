42ab0740c9806ed610f70ec7894ddb7d4e2ad730
kimjinmin82 <ggangi18@office.skhu.ac.kr>
2017-04-14 19:55:59 +0900

    commit test

--- app/src/main/java/com/example/hey/refrigerator/MainActivity.java
+++ app/src/main/java/com/example/hey/refrigerator/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.exam2);
+        setContentView(R.layout.exam);
     }
 }
